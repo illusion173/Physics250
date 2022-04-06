3b8584036a3a67a01385a291427bece3b672e139
illusion173 <illusion173@hotmail.com>
2022-04-06 09:32:56 -0400

    Added LCcircuit

---
+++ Physics250-ME30/LCCircuitMaxcurrent.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math
+
+def findMaxCurrent():
+    Henry = float(input('Input Henry: '))
+    Faraday = float(input('Faraday: '))
+    Charge = float(input('Charge: '))
+    current = float(input('Current: '))
+    #Conversions`
+    Henry = Henry / 1000
+    Faraday = Faraday * pow(10, -6)
+    Charge =  Charge * pow(10, -6)
+    current = current / 1000
+    #Hold on to your pants this is a pain in the ass
+    
+    Energy = (.5 * (pow(Charge, 2)/(Faraday))) + (.5 * Henry * pow(current, 2))
+    #note this is .5 * q^2/c + .5L*i^2
+    Imax = math.sqrt((2*Energy)/Henry) * pow(10, 3)
+    print(Imax)
+    
+findMaxCurrent()
--- Physics250-ME30/chargeOnSamePlate.py
+++ Physics250-ME30/chargeOnSamePlate.py
@@ -5,8 +5,20 @@ Esubo = 8.85 * pow(10,-12)
 
 
 def samePlate():
+    Henry = float(input('Input Henry (mH): '))
+    Faraday = float(input('Input Faraday: '))
+    Charge = float(input('Input Charge: '))
+    time = float(input('Input time: '))
+    #Conversions dont mind me
+    Henry = Henry / 1000
+    Faraday = Faraday * pow(10, -6)
+    Charge = Charge * pow(10, -9)
+    time = time / 1000
+    #Quick note, if youre doing this with a calculator, ensure youre in radians mode!
+    #Math.cos automatically assumes radian input
+    #pow(10,9) is for conversions and reader legibility.
+    newCharge = pow(10, 9) *Charge * math.cos((time)/(math.sqrt(Henry*Faraday)))
     
-    
-    
+    print(newCharge)
     
 samePlate()
